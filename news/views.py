00c9126f30261b62b6d820fc5480c5b49db1b7b4
Orwa400 <matheworwa3@gmail.com>
2024-10-17 00:16:35 +0300

    Add homepage

--- news/views.py
+++ news/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
+from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
@@ -27,3 +28,25 @@ def convert_dates(date):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_news(request, past_date):
+
+    try:
+
+        # Convet data from the string Url
+        date = dt.datetime.strptime(past_date, '%d-%m-%Y').date()
+
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+        day = convert_dates(date)
+        html = f'''
+            <html>
+                <body>
+                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                </body>
+            </html>
+        '''
+        return HttpResponse(html)
