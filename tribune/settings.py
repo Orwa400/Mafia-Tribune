7ac292997bcb169647f4ebb5298d2b9c0f0fa230
Orwa400 <matheworwa3@gmail.com>
2024-10-17 01:13:34 +0300

    Add navabar

--- tribune/settings.py
+++ tribune/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
-    'news',
-    'bootsrap3',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
