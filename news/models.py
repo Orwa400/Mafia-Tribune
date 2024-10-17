3fda2074d9da8da10de0d3f11107949d4744f7a4
Orwa400 <matheworwa3@gmail.com>
2024-10-17 22:33:40 +0300

    Add test cases

--- news/models.py
+++ news/models.py
@@ -11,6 +11,9 @@ class Editor(models.Model):
     def __str__(self):
         return self.first_name
 
+    def dave_editor(self):
+        self.save()
+
     class Meta:
         ordering = ['first_name']
 
@@ -23,7 +26,10 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setup(self):
+        self.mathew= editor(first_name = 'Mathew', last_name = 'Orwa', email = 'okumuorwa@gmail.com')
+
+    # Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mathew,Editor))
+
+    def test_save_method(self):
+        self.mathew.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > o)
+        
