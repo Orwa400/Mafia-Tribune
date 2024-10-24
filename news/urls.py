57debb3e61dd87787fac7c0fa1cd5b885330f790
Orwa400 <matheworwa3@gmail.com>
2024-10-24 21:28:55 +0300

    Add Image-Upload Logic

--- news/urls.py
+++ news/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('archives/(\d{4}-\d{2}-\d{2})/', views.past_days_news, name='pastNews'),
     path('search/', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
