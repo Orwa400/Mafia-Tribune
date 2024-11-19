f37a5212c32bc3a6dee0a169e2cb647992444c69
Orwa400 <matheworwa3@gmail.com>
2024-11-19 14:52:26 +0300

    Add single article logic

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.news_of_day, name='newsToday'),
-    path('archives/(\d{4}-\d{2}-\d{2})/', views.past_days_news, name='pastNews'),
+    path(r'archives/(\d{4}-\d{2}-\d{2})/', views.past_days_news, name='pastNews'),
     path('search/', views.search_results, name='search_results'),
+    path(r'article/(\d+)', views.article, name='article'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
