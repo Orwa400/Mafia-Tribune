c3d52b15f6ef53abdd81688ce46492ec0180ebee
Orwa400 <matheworwa3@gmail.com>
2024-11-20 13:00:02 +0300

    Add newsletter model

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
 from django.shortcuts import render, redirect
 from .models import Article
@@ -48,8 +48,11 @@ def news_today(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
-
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name, email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date": date, "news":news, "letterForm":form})
