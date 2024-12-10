71301506b84f6e2dc9495088a002e14774b5512e
Orwa400 <matheworwa3@gmail.com>
2024-12-10 15:45:46 +0300

    Update Article Form

--- tribune/urls.py
+++ tribune/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
     path(r'accounts/', include('django_registration.backends.activation.urls')),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),    
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),  
+    path(r'tinymce/', include('tinymce.urls')),  
 ]
